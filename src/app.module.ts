e5e9e040dc9e1bb776590a03a6d7b77c3b672f03
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-08 18:44:26 +0200

    solving conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,14 @@
-// src/app.module.ts
-
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from 'typeOrmConfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { ProductsModule } from './products/products.module';
 import { PaymentsModule } from './payments/payments.module';
 import { WithdrawalsModule } from './withdrawals/withdrawals.module';
 import { AuthenticationModule } from './authentication/authentication.module';
-=======
-import { PaymentsModule } from './payments/payments.module';
-import { WithdrawalsModule } from './withdrawals/withdrawals.module';
 import { SellerwalletModule } from './sellerwallet/sellerwallet.module';
->>>>>>> 0f1282c28758dcdb20a9b53b2bfc44f058febfc2
 
 @Module({
   imports: [
@@ -29,21 +22,11 @@ import { SellerwalletModule } from './sellerwallet/sellerwallet.module';
       inject: [ConfigService],
       useFactory: typeOrmConfig,
     }),
-
-<<<<<<< HEAD
     ProductsModule,
-
-=======
->>>>>>> 0f1282c28758dcdb20a9b53b2bfc44f058febfc2
     PaymentsModule,
-
     WithdrawalsModule,
-
-<<<<<<< HEAD
     AuthenticationModule,
-=======
     SellerwalletModule,
->>>>>>> 0f1282c28758dcdb20a9b53b2bfc44f058febfc2
   ],
   controllers: [AppController],
   providers: [AppService],

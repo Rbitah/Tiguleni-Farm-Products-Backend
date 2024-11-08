5f9ca5bacca221901115dc38714e8c1ff41de2c0
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-08 19:44:57 +0200

    created withdrawalDTO

--- src/withdrawals/dto/create-withdrawal.dto.ts
+++ src/withdrawals/dto/create-withdrawal.dto.ts
@@ -1,1 +1,17 @@
-export class CreateWithdrawalDto {}
+import { IsBoolean, IsDateString, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateWithdrawalDto {
+  @IsInt()
+  @IsNotEmpty()
+  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  mobile: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  amount: number;
+
+ 
+}

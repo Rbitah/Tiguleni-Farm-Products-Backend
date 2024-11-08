5b2a6feceb7c3b4f64f1697d179078f2381fbb52
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-08 18:48:33 +0200

    designed schema for authentication/user (maybe will be changed to user schema later)

--- src/authentication/entities/authentication.entity.ts
+++ src/authentication/entities/authentication.entity.ts
@@ -1,1 +1,20 @@
-export class Authentication {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "./role.enum";
+
+@Entity()
+export class Authentication {
+    @PrimaryGeneratedColumn('uuid')
+    user_Id:string
+
+    @Column({unique:true})
+    email:string
+
+    @Column()
+    password:string
+
+    @Column()
+    username:string
+
+    @Column({type:'enum',enum:Role, default:Role.BUYER})
+    role:Role
+}

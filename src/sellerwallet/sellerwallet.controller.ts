946be400b68fc15e7eebc57bb0e3ce0598ef2345
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-09 12:18:24 +0200

    created route for get indiavidual sellerwallet

--- src/sellerwallet/sellerwallet.controller.ts
+++ src/sellerwallet/sellerwallet.controller.ts
@@ -1,22 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Body } from '@nestjs/common';
 import { SellerwalletService } from './sellerwallet.service';
-import { CreateSellerwalletDto } from './dto/create-sellerwallet.dto';
-import { UpdateSellerwalletDto } from './dto/update-sellerwallet.dto';
 
 @Controller('sellerwallet')
 export class SellerwalletController {
   constructor(private readonly sellerwalletService: SellerwalletService) {}
 
-  @Post()
-  create(@Body() createSellerwalletDto: CreateSellerwalletDto) {
-    return this.sellerwalletService.create(createSellerwalletDto);
+  @Get()
+  sellerWallet(@Body() userId: string) {
+    return this.sellerwalletService.sellerWallet(userId);
   }
-
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.sellerwalletService.findOne(+id);
-  }
-
- 
 }
--- src/sellerwallet/sellerwallet.service.ts
+++ src/sellerwallet/sellerwallet.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSellerwalletDto } from './dto/create-sellerwallet.dto';
 import { UpdateSellerwalletDto } from './dto/update-sellerwallet.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Sellerwallet } from './entities/sellerwallet.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SellerwalletService {
-  create(createSellerwalletDto: CreateSellerwalletDto) {
-    return 'This action adds a new sellerwallet';
-  }
+  constructor(
+    @InjectRepository(Sellerwallet)
+    private readonly sellerwalletRepository: Repository<Sellerwallet>,
+  ) {}
 
- 
-  findOne(id: number) {
-    return `This action returns a #${id} sellerwallet`;
+  async sellerWallet(userId: string) {
+    let sellerwallet = await this.sellerwalletRepository.findOne({
+      where: { seller: { userId: userId } },
+    });
+    if (!sellerwallet) {
+      sellerwallet = this.sellerwalletRepository.create({
+        seller: sellerwallet.seller,
+        mainWalletBalance: 0,
+        totalNumberOfSales: 0,
+        totalSales: 0,
+        totalNumberOfWithdrawals: 0,
+        totalCashOut: 0,
+      });
+    }
+    await this.sellerwalletRepository.save(sellerwallet);
+    return { sellerwallet };
   }
-
-  
 }

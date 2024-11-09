e23c351c07844052df581941d9b98744a861b32d
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-09 14:58:46 +0200

    configure mail service to use nodemailer to send emails like signup welcome email and reset passowrd emails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(
+    private transporter: nodemailer.Transporter,
+    private readonly configService: ConfigService,
+  ) {
+    this.transporter = nodemailer.createTransport({
+      host: this.configService.get('HOST'),
+      port: this.configService.get('PORT'),
+      secure: this.configService.get('SECURE'),
+      auth: {
+        user: this.configService.get('EMAIL_USER'),
+        pass: this.configService.get('EMAIL_PASS'),
+      },
+    });
+  }
+}

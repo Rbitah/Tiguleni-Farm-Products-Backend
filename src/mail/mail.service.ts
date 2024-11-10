d9fedb2cfe71eeb3bc5b0b66964702f496874652
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-10 17:21:35 +0200

    fixed mailservice

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -3,17 +3,18 @@ import { ConfigService } from '@nestjs/config';
 import * as nodemailer from 'nodemailer';
 @Injectable()
 export class MailService {
+  private transporter: nodemailer.Transporter
   constructor(
-    private transporter: nodemailer.Transporter,
+    
     private readonly configService: ConfigService,
   ) {
     this.transporter = nodemailer.createTransport({
-      host: this.configService.get('HOST'),
-      port: this.configService.get('PORT'),
-      secure: this.configService.get('SECURE'),
+      host: this.configService.get<string>('HOST'),
+      port: this.configService.get<number>('PORT'),
+      secure: this.configService.get<boolean>('SECURE'),
       auth: {
-        user: this.configService.get('EMAIL_USER'),
-        pass: this.configService.get('EMAIL_PASS'),
+        user: this.configService.get<string>('EMAIL_USER'),
+        pass: this.configService.get<string>('EMAIL_PASS'),
       },
     });
   }

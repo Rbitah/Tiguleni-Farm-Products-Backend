72526f276299f3789a027665f4730a28c40cb5d7
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-10 17:21:59 +0200

    imported entities/schemas

--- typeOrmConfig.ts
+++ typeOrmConfig.ts
@@ -1,5 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { User } from 'src/authentication/entities/authentication.entity';
+import { Payment } from 'src/payments/entities/payment.entity';
+import { Withdrawal } from 'src/withdrawals/entities/withdrawal.entity';
+import { Sellerwallet } from 'src/sellerwallet/entities/sellerwallet.entity';
+import { Products } from 'src/products/products.entity';
+import { Token } from 'src/authentication/entities/reset.entity';
 
 export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'mysql',
@@ -8,7 +14,7 @@ export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOption
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_NAME'),
-  entities: [],
+  entities: [User,Payment,Withdrawal,Sellerwallet,Products,Token],
   synchronize: false,
   logging: true,
 });

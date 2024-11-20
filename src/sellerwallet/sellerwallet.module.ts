d6c48ee8047f2eca0f03d749dc3c2daf5a40aacf
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-20 18:02:36 +0200

    imported configservice to access env's

--- src/sellerwallet/sellerwallet.module.ts
+++ src/sellerwallet/sellerwallet.module.ts
@@ -3,9 +3,17 @@ import { SellerwalletService } from './sellerwallet.service';
 import { SellerwalletController } from './sellerwallet.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Sellerwallet } from './entities/sellerwallet.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Sellerwallet])],
+  imports:[TypeOrmModule.forFeature([Sellerwallet]),JwtModule.registerAsync({
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn:configService.get<string>('JWT_EXPIRATION') },
+    }),
+    inject: [ConfigService],
+  }),],
   controllers: [SellerwalletController],
   providers: [SellerwalletService],
 })

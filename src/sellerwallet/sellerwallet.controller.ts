46108a61c4dd1e970d0cbbb8fe7ff1655b7a0be5
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-20 18:03:30 +0200

    protected sellerwalled using guards and roles

--- src/sellerwallet/sellerwallet.controller.ts
+++ src/sellerwallet/sellerwallet.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Body } from '@nestjs/common';
+import { Controller, Get, Body, UseGuards, Req } from '@nestjs/common';
 import { SellerwalletService } from './sellerwallet.service';
+import { AuthGuard } from 'src/authentication/guards/authentication.guard';
+import { RoleGuardAuth } from 'src/authentication/guards/roles.guard';
+import { Roles } from 'src/authentication/decorator/roles.decorator';
 
 @Controller('sellerwallet')
 export class SellerwalletController {
   constructor(private readonly sellerwalletService: SellerwalletService) {}
 
+  @UseGuards(AuthGuard,RoleGuardAuth)
+  @Roles(['seller'])
   @Get()
-  sellerWallet(@Body() userId: string) {
+  sellerWallet(@Req() req) {
+    const userId =req.userId
     return this.sellerwalletService.sellerWallet(userId);
   }
 }

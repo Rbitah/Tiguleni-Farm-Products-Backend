20972f5996d5f848234230a63720e17e8b08cdb3
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-25 19:21:20 +0200

    added endpoints for getting withdrawal list

--- src/withdrawals/withdrawals.controller.ts
+++ src/withdrawals/withdrawals.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { WithdrawalsService } from './withdrawals.service';
 import { CreateWithdrawalDto } from './dto/create-withdrawal.dto';
 import { UpdateWithdrawalDto } from './dto/update-withdrawal.dto';
+import { AuthGuard } from 'src/authentication/guards/authentication.guard';
+import { RoleGuardAuth } from 'src/authentication/guards/roles.guard';
+import { Roles } from 'src/authentication/decorator/roles.decorator';
 
 @Controller('withdrawals')
 export class WithdrawalsController {
@@ -11,5 +24,22 @@ export class WithdrawalsController {
   cashoutMobile(@Body() createWithdrawalDto: CreateWithdrawalDto) {
     return this.withdrawalsService.cashoutMobile(createWithdrawalDto);
   }
- 
+
+  @UseGuards(AuthGuard, RoleGuardAuth)
+  @Roles(['seller'])
+  @Get('seller-withdrwals')
+  async getWithdrawalsList(@Req() req) {
+    const userId = req.userId;
+    const withdrawals =
+      await this.withdrawalsService.getWithdrawalsList(userId);
+    return withdrawals;
+  }
+
+  @UseGuards(AuthGuard, RoleGuardAuth)
+  @Roles(['admin'])
+  @Get('admin-withdrawals')
+  async getAdminWithdrawalsList() {
+    const withdrawals = await this.withdrawalsService.adminWithdrawalsList();
+    return withdrawals;
+  }
 }
--- src/withdrawals/withdrawals.module.ts
+++ src/withdrawals/withdrawals.module.ts
@@ -6,9 +6,17 @@ import { User } from 'src/authentication/entities/authentication.entity';
 import { Withdrawal } from './entities/withdrawal.entity';
 import { Sellerwallet } from 'src/sellerwallet/entities/sellerwallet.entity';
 import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User,Withdrawal,Sellerwallet]),HttpModule],
+  imports:[TypeOrmModule.forFeature([User,Withdrawal,Sellerwallet]),HttpModule,JwtModule.registerAsync({
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn:configService.get<string>('JWT_EXPIRATION') },
+    }),
+    inject: [ConfigService],
+  })],
   controllers: [WithdrawalsController],
   providers: [WithdrawalsService],
 })

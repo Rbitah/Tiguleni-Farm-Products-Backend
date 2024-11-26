397a170510b0e4053e78eba5754a2a268cb60bd9
Rbitah <bsc-35-21@unima.ac.mw>
2024-11-26 19:57:28 +0200

    conected to online database

--- typeOrmConfig.ts
+++ typeOrmConfig.ts
@@ -12,12 +12,12 @@ import { Profile } from 'src/userprofile/entities/userprofile.entity';
 
 export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'mysql',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_NAME'),
-  // url:configService.get<string>('MYSQL_PUBLIC_URL'),  for online database
+  // host: configService.get<string>('DB_HOST'),
+  // port: configService.get<number>('DB_PORT'),
+  // username: configService.get<string>('DB_USERNAME'),
+  // password: configService.get<string>('DB_PASSWORD'),
+  // database: configService.get<string>('DB_NAME'),
+  url:configService.get<string>('MYSQL_PUBLIC_URL'), 
   entities: [User,Payment,Withdrawal,Sellerwallet,Products,Token,Sales,Feedback,Profile],
   synchronize:true,
   logging: true,
